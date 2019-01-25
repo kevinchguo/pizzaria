24149b789c19e1435e5c16f4d2a103a66d39abba
Kevinnnface <45556758+Kevinnnface@users.noreply.github.com>
2019-01-25 02:42:33 -1000

    desc

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
-window.onscroll = function() {myFunction()};
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
   } else {
-    navbar.classList.remove("sticky");
+    document.getElementById("myBtn").style.display = "none";
   }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  // document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+var Pizza = {
+  name: kevin,
 }
