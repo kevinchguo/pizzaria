c14fac05b2f64e8487b86776531b635064455c0e
Kevinnnface <45556758+Kevinnnface@users.noreply.github.com>
2019-01-25 17:20:26 -1000

    asdf

--- js/app.js
+++ js/app.js
@@ -4,12 +4,27 @@ var navbar = document.getElementById("topMenu");
 console.log(navbar)
 var sticky = navbar.offsetTop;
 console.log(sticky)
-window.pageYOffset = document.getElementById('today')
 
 function myFunction() {
-  if (window.pageYOffset > sticky) {
+  if (window.pageYOffset >= sticky) {
     navbar.classList.add('sticky')
   }else{
     navbar.classList.remove('sticky');
   }
 }
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("myBtn").style.display = "block";
+//   } else {
+//     document.getElementById("myBtn").style.display = "none";
+//   }
+// }
+
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
